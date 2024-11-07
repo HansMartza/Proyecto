bb43f4503205a47ff9294627687be5eb704822ca
HansMartza <113564303+HansMartza@users.noreply.github.com>
2024-11-07 08:27:15 -0600

    modifique

---
+++ Hanso/src/main/java/mx/unam/fi/hanso/Hanso.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.unam.fi.hanso;
+
+/**
+ *
+ * @author Hans
+ */
+public class Hanso {
+
+    public static void main(String[] args) {
+        System.out.println("Holaaaa");
+    }
+}
