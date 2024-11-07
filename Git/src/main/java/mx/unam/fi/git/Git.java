080c50eb84b667539a3b23ffb56ccde10c732354
HansMartza <113564303+HansMartza@users.noreply.github.com>
2024-11-07 08:42:19 -0600

    modifique

---
+++ Git/src/main/java/mx/unam/fi/git/Git.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.unam.fi.git;
+
+/**
+ *
+ * @author Hans
+ */
+public class Git {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Holaaaaaa");
+    }
+}
