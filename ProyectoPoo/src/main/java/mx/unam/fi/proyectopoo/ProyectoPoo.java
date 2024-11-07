f2c619b4917e6ca71537ef57a47ffa70b569b5e1
HansMartza <113564303+HansMartza@users.noreply.github.com>
2024-11-07 09:13:28 -0600

    modifique

---
+++ ProyectoPoo/src/main/java/mx/unam/fi/proyectopoo/ProyectoPoo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.unam.fi.proyectopoo;
+
+/**
+ *
+ * @author Hans
+ */
+public class ProyectoPoo {
+
+    public static void main(String[] args) {
+        System.out.println("Aqui sera el proyecto");
+    }
+}
